1c72d2f17f1965699d91a663a763aadb23ab01a3
LeeHyunseok <smartbosslee@gmail.com>
2015-06-28 14:35:44 +0900

    구글 애널리틱스 추가.

--- views/layouts/default.blade.php
+++ views/layouts/default.blade.php
@@ -19,6 +19,18 @@
     <![endif]-->
 </head>
 <body>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-64574240-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
@@ -59,5 +71,6 @@
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
 </body>
 </html>
